c1b9b0bd4c8967036c956d0dc39b2bf07741129d
lucagallotta95 <lucagallotta95@gmail.com>
2024-12-05 21:43:25 +0100

    Passo2

--- src/App.java
+++ src/App.java
@@ -1,35 +1,53 @@
 import java.util.Random;
 
-
 public class App {
     public static void main(String[] args) throws Exception {
 
-        int punteggioUtente, punteggioPC, lancioUtente1, lancioUtente2, lancioPC1, lancioPC2;
         Random rdn = new Random();
+
+        int punteggioUtente, punteggioPC, lancioUtente1, lancioUtente2, lancioPC1, lancioPC2, turnoUtente, turnoPC;
         lancioUtente1 = rdn.nextInt(6) + 1;
         lancioUtente2 = rdn.nextInt(6) + 1;
         lancioPC1 = rdn.nextInt(6) + 1;
         lancioPC2 = rdn.nextInt(6) + 1;
         punteggioUtente = lancioUtente1 + lancioUtente2;
         punteggioPC = lancioPC1 + lancioPC2;
+        turnoUtente = 0;
+        turnoPC = 0;
 
-        System.out
-                .println("Utente ha lanciato " + lancioUtente1 + " e " + lancioUtente2 + " totale " + punteggioUtente);
+        for (int index = 0; index < 3; index++) {
 
-        System.out.println("PC ha lanciato " + lancioPC1 + " e " + lancioPC2 + " totale " + punteggioPC);
+            System.out
+                    .println("Utente ha lanciato " + lancioUtente1 + " e " + lancioUtente2 + " totale "
+                            + punteggioUtente);
 
-        if (punteggioUtente > punteggioPC) {
-            System.out.println("Ha vinto l'utente");
+            System.out.println("PC ha lanciato " + lancioPC1 + " e " + lancioPC2 + " totale " + punteggioPC);
 
-        } else if (punteggioUtente == punteggioPC) {
-            System.out.println("Pareggio");
+            if (punteggioUtente > punteggioPC) {
+                System.out.println("Ha vinto l'utente");
 
-        } else {
-            System.out.println("Vince il PC");
+            } else if (punteggioUtente == punteggioPC) {
+                System.out.println("Pareggio");
+
+            } else {
+                System.out.println("Vince il PC");
+                turnoPC++;
+
+            }
+
+        }   if (turnoUtente > turnoPC) {
+                System.out.println("L'utente ha vinto la partita");
+            
+        }   else if (turnoUtente == turnoPC) {
+                System.out.println("Pareggio");
+
+        } else { 
+                System.out.println("Vince il PC");
 
         }
 
+
         
 
-    }
+    }   
 }
